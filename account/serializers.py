a82f871b98dfabfe5c9ca789d5ab7b469615130f
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-11 02:46:48 +0530

    Integrated more serializations

--- account/serializers.py
+++ account/serializers.py
@@ -1,7 +1,29 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
+from rest_framework import serializers
+from account.models import User
+from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+  # We are writing this becoz we need confirm password field in our Registratin Request
+  password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
+  class Meta:
+    model = User
+    fields=['email', 'name', 'password', 'password2', 'tc']
+    extra_kwargs={
+      'password':{'write_only':True}
+    }
+
+  # Validating Password and Confirm Password while Registration
+  def validate(self, attrs):
+    password = attrs.get('password')
+    password2 = attrs.get('password2')
+    if password != password2:
+      raise serializers.ValidationError("Password and Confirm Password doesn't match")
+    return attrs
+
+  def create(self, validate_data):
+    return User.objects.create_user(**validate_data)
--- account/views.py
+++ account/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import UserRegistrationSerializer
+from .models import User
 
 class Register(APIView):
-    def get(self, request):
-        return Response({'message': 'Hello, World!'}, status=status.HTTP_200_OK)
+    def post(self, request):
+        serialize = UserRegistrationSerializer(data=request.data)
+        if serialize.is_valid(raise_exception=True):
+            serialize.save()
+            return Response({'status': 'ok', 'message' : 'User created successfully', 'data' : serialize.data}, status=status.HTTP_200_OK)
+        return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
