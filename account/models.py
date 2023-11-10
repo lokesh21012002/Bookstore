a8db1b1e610fc5bcd40f8c4d61dcf1b144809d4f
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-11 01:41:21 +0530

    Serializer, Models, Admin

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255)
+    name = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'email'
+
