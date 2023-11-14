972e1fab69e0113930a6e111fca3c9c7dd644f9d
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-14 18:52:04 +0530

    Added book app & registered in the admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'author', 'price', 'totalsold', 'totalavailable', 'genre', 'created_at', 'updated_at')
