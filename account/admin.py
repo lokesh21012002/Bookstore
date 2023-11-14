b88d3d0bb87751c598a9656657b9e4a51a77c148
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-14 18:54:47 +0530

    Registered more models

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import User
+from .models import User, PhoneNumber, Buyer, Seller, Orders
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id','email', 'password', 'name', 'created_at', 'updated_at')
+
+@admin.register(PhoneNumber)
+class PhoneNumberAdmin(admin.ModelAdmin):
+    list_display = ('id','user', 'phone_number', 'created_at', 'updated_at')
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('id','user', 'city', 'state', 'country', 'landmark', 'created_at', 'updated_at')
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('id','user', 'storename', 'productsold', 'created_at', 'updated_at')
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id','buyer', 'seller', 'book', 'address', 'status', 'total', 'quantity', 'totalamount', 'created_at', 'updated_at')
