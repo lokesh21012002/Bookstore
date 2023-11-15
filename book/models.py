337e828c22bff66af5967e6e0d5ff41599632d8b
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-16 00:53:02 +0530

    deletion

--- book/models.py
+++ book/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Book(models.Model):
+    seller = models.ForeignKey('account.Seller', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.IntegerField()
--- book/serializers.py
+++ book/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from account.serializers import SellerSerializer
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    seller = SellerSerializer(read_only=True, required=False)
     class Meta:
         model = Book
         fields = '__all__'
