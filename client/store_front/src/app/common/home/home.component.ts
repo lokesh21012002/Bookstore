ada1ff656c99de0400d9ac294dcfa2e45aca922b
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-20 21:06:11 +0530

    Seller, Buyer Home Added

--- client/store_front/src/app/common/home/home.component.ts
+++ client/store_front/src/app/common/home/home.component.ts
@@ -15,6 +15,89 @@ export class HomeComponent {
   userdata: any = {};
   roledata: any = {};
 
+  bookdata = [
+    {
+      id : 1,
+      title : "Marvel",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"  
+    },
+    {
+      id : 2,
+      title : "Hulk",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    },
+    {
+      id : 3,
+      title : "Thor",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    },
+    {
+      id : 4,
+      title : "Captain America",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    },
+    {
+      id : 5,
+      title : "Iron Man",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    },
+    {
+      id : 6,
+      title : "Black Panther",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    },
+    {
+      id : 7,
+      title : "Spider Man",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    },
+    {
+      id : 8,
+      title : "Ant Man",
+      author : "Stan Lee",
+      price : 100,
+      totalsold : 1,
+      totalavailable : 10,
+      genre : "Comic",
+      cover : "https://upload.wikimedia.org/wikipedia/en/0/0e/Marvel_Logo.svg"
+    }
+  ];
+
   constructor(private accountservice : AccountService) { } 
 
   register(){
