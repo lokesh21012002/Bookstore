7e85d969622f981040454706f9b20558d4e68e46
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-20 14:45:58 +0530

    api testing

--- client/store_front/src/app/app.module.ts
+++ client/store_front/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './common/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/store_front/src/app/common/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { login } from '../../../interfaces/datatype';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  constructor() { } 
+
+  logIn(data : login){
+  
+  }
+
+}
