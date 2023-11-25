1e19602013a4853d383b3d93f0d1f561ee8bb04e
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-11-25 23:30:53 +0530

    Seller service toast added

--- client/store_front/src/app/services/book/book.service.ts
+++ client/store_front/src/app/services/book/book.service.ts
@@ -2,26 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, concatMap, of } from 'rxjs';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  private allbookSubject = new BehaviorSubject<any>([])
-  public bookData$ = this.allbookSubject.asObservable()
+  private allbookSubject = new BehaviorSubject<any>([]);
+  public bookData$ = this.allbookSubject.asObservable();
 
-  private searchbookSubject = new BehaviorSubject<any>([])
-  public searchbookData$ = this.searchbookSubject.asObservable()
+  private searchbookSubject = new BehaviorSubject<any>([]);
+  public searchbookData$ = this.searchbookSubject.asObservable();
 
-  constructor(private http : HttpClient, private router : Router) { }
+  constructor(private http: HttpClient, private router: Router, private toastr: ToastrService) { }
 
-  getAllBooksApi(){
-    this.http.get("http://localhost:8000/api/v1/book/bookapi/", {'observe' : 'response'}).subscribe((response) => {
+  getAllBooksApi() {
+    this.http.get("http://localhost:8000/api/v1/book/bookapi/", { 'observe': 'response' }).subscribe((response) => {
 
       const httpresponse = response;
 
-      if(httpresponse.status !== 200) {
+      if (httpresponse.status !== 200) {
+        this.toastr.error('An unexpected error occurred while fetching books.', 'Error');
         return;
       }
 
@@ -32,16 +34,18 @@ export class BookService {
 
       localStorage.setItem('bookData', JSON.stringify(books));
 
-    })
+    }, error => {
+      this.toastr.error('An unexpected error occurred while fetching books.', 'Error');
+      console.error(error);
+    });
   }
 
-  getBookByIdApi(id : number){
+  getBookByIdApi(id: number) {
     const url = "http://localhost:8000/api/v1/book/bookapi/{id}".replace("{id}", id.toString());
-    return this.http.get(url, {'observe' : 'response'});
+    return this.http.get(url, { 'observe': 'response' });
   }
 
-  createBookApi(data : any, token: any){
-
+  createBookApi(data: any, token: any) {
     let formData = new FormData();
 
     formData.append('title', data.title);
@@ -52,136 +56,145 @@ export class BookService {
     formData.append('genre', data.genre);
     formData.append('cover', data.cover);
 
-    this.http.post("http://localhost:8000/api/v1/book/bookapi/", formData, {observe : 'response', headers : {
-      'Authorization' : 'Bearer ' + token,
-    }}).subscribe((response) => {
+    if (!data.title || !data.author || !data.price || !data.totalsold || !data.totalavailable || !data.genre || !data.cover) {
+      this.toastr.error('Please fill in all required fields.', 'Error');
+      return;
+    }
+
+    this.http.post("http://localhost:8000/api/v1/book/bookapi/", formData, { observe: 'response', headers: { 'Authorization': 'Bearer ' + token } }).subscribe((response) => {
 
       const httpresponse = response;
 
-      if(httpresponse.status !== 201) {
+      if (httpresponse.status !== 200) {
+        this.toastr.error('An unexpected error occurred while creating the book.', 'Error');
         return;
       }
 
       this.router.navigate(['Home/MyBooks']);
+      this.toastr.success('Book created successfully.', 'Success');
 
-    })
+    }, error => {
+      this.toastr.error('An unexpected error occurred while creating the book.', 'Error');
+      console.error(error);
+    });
   }
 
-  getSellerBooksApi(token: any){
-    
-    return this.http.get("http://localhost:8000/api/v1/book/bookapi/seller/", {headers : {
-      'Authorization' : 'Bearer ' + token,
-    }, 'observe' : 'response'});
-
+  getSellerBooksApi(token: any) {
+    return this.http.get("http://localhost:8000/api/v1/book/bookapi/seller/", { headers: { 'Authorization': 'Bearer ' + token }, 'observe': 'response' });
   }
 
-  editBookApi(id : number, data : any, token : any){
-    
+  editBookApi(id: number, data: any, token: any) {
     const url = "http://localhost:8000/api/v1/book/bookapi/{id}".replace("{id}", id.toString());
 
     let formData = new FormData();
 
-    if(data.title){
+    if (data.title) {
       formData.append('title', data.title);
     }
-    if(data.author){
+    if (data.author) {
       formData.append('author', data.author);
     }
-    if(data.price){
+    if (data.price) {
       formData.append('price', data.price);
     }
-    if(data.totalsold){
+    if (data.totalsold) {
       formData.append('totalsold', data.totalsold);
     }
-    if(data.totalavailable){
+    if (data.totalavailable) {
       formData.append('totalavailable', data.totalavailable);
     }
-    if(data.genre){
+    if (data.genre) {
       formData.append('genre', data.genre);
     }
-    if(data.cover){
+    if (data.cover) {
       formData.append('cover', data.cover);
     }
 
-    console.warn("HELLo");
-    
+    if (!data.title && !data.author && !data.price && !data.totalsold && !data.totalavailable && !data.genre && !data.cover) {
+      this.toastr.error('Please provide at least one field to update.', 'Error');
+      return;
+    }
 
-    this.http.put(url, formData, {headers : {
-      'Authorization' : 'Bearer ' + token,
-    }, 'observe' : 'response'}).subscribe((response) => {
+    this.http.put(url, formData, { headers: { 'Authorization': 'Bearer ' + token }, 'observe': 'response' }).subscribe((response) => {
       this.router.navigate(['Home/MyBooks']);
-    })
-
+      this.toastr.success('Book updated successfully.', 'Success');
+    }, error => {
+      this.toastr.error('An unexpected error occurred while updating the book.', 'Error');
+      console.error(error);
+    });
   }
 
-  getSearchBooksApi(searchQuery : any){
-
+  getSearchBooksApi(searchQuery: any) {
     const url1 = `http://localhost:8000/api/v1/book/bookapi/?genre=${searchQuery}`;
     const url2 = `http://localhost:8000/api/v1/book/bookapi/?title=${searchQuery}`;
     const url3 = `http://localhost:8000/api/v1/book/bookapi/?author=${searchQuery}`;
 
     let flag = 0;
 
-    this.http.get(url1, {'observe' : 'response'}).subscribe((response) => {
+    this.http.get(url1, { 'observe': 'response' }).subscribe((response) => {
 
       const httpresponse: any = response;
-      if(httpresponse.status !== 200) {
+      if (httpresponse.status !== 200) {
         return;
       }
       const jsondata: any = httpresponse.body;
       this.searchbookSubject.next(jsondata.data)
 
-      if(jsondata.data.length > 0)
+      if (jsondata.data.length > 0)
         flag = 1
 
     })
 
-    if(flag === 0){
+    if (flag === 0) {
 
-      this.http.get(url2, {'observe' : 'response'}).subscribe((response) => {
+      this.http.get(url2, { 'observe': 'response' }).subscribe((response) => {
 
         const httpresponse: any = response;
-        if(httpresponse.status !== 200) {
+        if (httpresponse.status !== 200) {
           return;
         }
         const jsondata: any = httpresponse.body;
-        
+
         this.searchbookSubject.next(jsondata.data)
 
-        if(jsondata.data.length > 0)
+        if (jsondata.data.length > 0)
           flag = 1
 
-    })}
+      })
+    }
 
-    if(flag === 0){
+    if (flag === 0) {
 
-      this.http.get(url3, {'observe' : 'response'}).subscribe((response) => {
+      this.http.get(url3, { 'observe': 'response' }).subscribe((response) => {
 
         const httpresponse: any = response;
-        if(httpresponse.status !== 200) {
+        if (httpresponse.status !== 200) {
           return;
         }
         const jsondata: any = httpresponse.body;
         this.searchbookSubject.next(jsondata.data)
 
-        if(jsondata.data.length > 0)
+        if (jsondata.data.length > 0)
           flag = 1
-    })}
-
+      })
+    }
   }
 
-  deleteBookApi(id : number, token: any){
-    
+  deleteBookApi(id: number, token: any) {
     const url = "http://localhost:8000/api/v1/book/bookapi/{id}".replace("{id}", id.toString());
 
-    this.http.delete(url, {headers : {
-      'Authorization' : 'Bearer ' + token,
-    }, 'observe' : 'response'}).subscribe((response) => {
+    this.http.delete(url, { headers: { 'Authorization': 'Bearer ' + token }, 'observe': 'response' }).subscribe((response) => {
+      if (response.status !== 200) {
+        this.toastr.error('An unexpected error occurred while deleting the book.', 'Error');
+        return;
+      }
+      this.toastr.success('Book deleted successfully.', 'Success');
+      this.router.navigate(['Home/MyBooks']);
 
-    this.router.navigate(['Home/MyBooks']);
-  
-  })
-  
+    }, error => {
+      this.toastr.error('An unexpected error occurred while deleting the book.', 'Error');
+      console.error(error);
+    });
   }
 
 }
